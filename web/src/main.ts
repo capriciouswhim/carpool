f5f1d1822c9c429bb10329008ff7a7bd18015035
dfranz <dfranz@amanaacademy.org>
2025-09-03 13:36:17 -0400

    disable double-tap to zoom

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,3 +4,6 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+/* catch unhandled dbl-click events on iOS */
+document.addEventListener('dblclick', ($event) => { $event.preventDefault() })
