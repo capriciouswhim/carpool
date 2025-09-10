dac59eb562a7d700dc88623931e34fb6528b8ce6
dfranz <dfranz@amanaacademy.org>
2025-09-10 12:07:47 -0400

    Fix unconfigured passport module

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "node:fs";
-
+import { User } from "src/model/user.model.js";
 import { envs } from "./envs/index.js";
 import loggerConfig from "./logger/index.js";
 const pkg = JSON.parse(readFileSync("./package.json", { encoding: "utf8" }));
@@ -9,6 +9,10 @@ export const config: Partial<TsED.Configuration> = {
   envs,
   ajv: {
     returnsCoercedValues: true
+  },
+  passport: {
+    disableSession: true,
+    userInfoModel: User
   },
   logger: loggerConfig
   // additional shared configuration
