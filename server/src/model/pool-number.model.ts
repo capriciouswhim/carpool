a6ad34d8c853f424935cf3f4d558215dd261fb43
dfranz <dfranz@amanaacademy.org>
2025-09-15 17:41:04 -0400

    bug - recall time missing from pool number model

--- server/src/model/pool-number.model.ts
+++ server/src/model/pool-number.model.ts
@@ -2,6 +2,7 @@ export class PoolNumber {
   pool_number   :number = 0
   lane_time     :string = '0000-00-00'
   call_time     :string | null  = null
+  recall_time   :string | null  = null
   send_time     :string | null  = null
   exit_time     :string | null  = null
   gone_time     :string | null  = null
