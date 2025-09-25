e0fa37fbc3d89a8e9d6cdd7a5eb344ee95111002
dfranz <dfranz@amanaacademy.org>
2025-09-25 12:20:39 -0400

    label pool numbers

--- client/src/component/pool-number.component/pool-number.component.ts
+++ client/src/component/pool-number.component/pool-number.component.ts
@@ -8,18 +8,53 @@ import { TypedPoolNumber } from '../../model';
   styleUrl: './pool-number.component.scss'
 })
 export class PoolNumberComponent {
+  showLabel = input<boolean>(true)
   poolNumber = input.required<TypedPoolNumber>()
   invoke = output<TypedPoolNumber>()
 
   getClass = (poolNumberSignal: InputSignal<TypedPoolNumber>) => {
     const poolNumber = poolNumberSignal()
+    let result
     switch(poolNumberSignal().state) {
-      case 'LANE': return { lane: true }
-      case 'CALL': return { call: true }
-      case 'RECALL': return { recall: true }
-      case 'SEND': return { send: true }
-      case 'EXIT': return { exit: true }
-      case 'GONE': return { gone: true }
+      case 'LANE':
+        result = { lane: true }
+        break;
+      case 'CALL':
+        result = { call: true }
+        break;
+      case 'RECALL':
+        result = { recall: true }
+        break;
+      case 'SEND':
+        result = { send: true }
+        break;
+      case 'EXIT':
+        result = { exit: true }
+        break;
+      case 'GONE':
+        result = { gone: true }
+        break;
+    }
+    
+    return {
+      showlabel: this.showLabel(),
+      ...result
+    }
+  }
+
+  getHumanLabel = (poolNumberSignal: InputSignal<TypedPoolNumber>) => {
+    if(!this.showLabel()) {
+      return ''
+    }
+
+    const poolNumber = poolNumberSignal()
+    switch(poolNumberSignal().state) {
+      case 'LANE': return 'Car Has Arrived'
+      case 'CALL': return 'Number Called'
+      case 'RECALL': return 'Number Called Again'
+      case 'SEND': return 'Student Sent To Door'
+      case 'EXIT': return 'Student Is Outside'
+      case 'GONE': return 'Car Has Left'
     }
   }
 
