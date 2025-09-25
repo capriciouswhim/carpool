b8898aef98bf9e2b0c52bf7ea3eed647c67f88d0
dfranz <dfranz@amanaacademy.org>
2025-09-25 13:48:35 -0400

    wire-up reset page

--- client/src/page/reset.page/reset.page.ts
+++ client/src/page/reset.page/reset.page.ts
@@ -1,16 +1,20 @@
-import { Component, inject } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { Store } from "@ngrx/store";
-import { carpoolAction } from "../../store";
+import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { PoolNumberComponent } from '../../component';
+import { BasePage } from '../base.page/base.page';
+import { TypedPoolNumber } from '../../model';
+import { MatButtonModule } from '@angular/material/button';
+import { carpoolAction } from '../../store';
 
 @Component({
-    imports: [  MatButtonModule ],
-    template: '<button mat-button (click)="onClick()">Reset</button>',
+  selector: 'car-reset-page',
+  imports: [AsyncPipe, MatButtonModule, PoolNumberComponent],
+  templateUrl: './reset.page.html',
+  styleUrl: './reset.page.scss'
 })
-export class ResetPage {
-    store = inject(Store)
+export class ResetPage extends BasePage {
 
-    onClick() {
-        this.store.dispatch(carpoolAction.reset())
-    }
+  onClick = () =>
+    this.store.dispatch(carpoolAction.reset())
+
 }
