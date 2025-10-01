2b54bb030c01c2bba21fdb850fc7c89c714fb0d6
dfranz <dfranz@amanaacademy.org>
2025-09-30 21:01:46 -0400

    remove dead code

--- client/src/component/header.component/header.component.ts
+++ client/src/component/header.component/header.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from '@angular/material/menu'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from "@angular/material/icon";
 import { GoogleComponent } from '../google.component/google.component';
 
 @Component({
   selector: 'car-header',
-  imports: [GoogleComponent, MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [GoogleComponent, MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  readonly data_client_id="645008399704-qqnqru9a5ivoaus70nqg8nihucn2qruq.apps.googleusercontent.com"
-
-  ngAfterViewInit() {
-    const element = window.document.getElementById('g_id_onload')
-    element?.setAttribute('data-client_id', this.data_client_id)
-  }
-}
+export class HeaderComponent {}
