981fbe319088e49ebafed47ae39d8ea0db505f92
dfranz <dfranz@amanaacademy.org>
2025-10-01 01:59:08 -0400

    variable headings

--- client/src/component/header.component/header.component.ts
+++ client/src/component/header.component/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterModule } from '@angular/router';
 import { MatButtonModule } from "@angular/material/button";
 import { MatMenuModule } from '@angular/material/menu'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from "@angular/material/icon";
 import { GoogleComponent } from '../google.component/google.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'car-header',
@@ -12,4 +13,24 @@ import { GoogleComponent } from '../google.component/google.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  router = inject(Router)
+  title = signal('Carpool')
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        switch(event.urlAfterRedirects) {
+          case '/arrivals':
+            this.title.set('Arrivals')
+            break;
+          case '/departures':
+            this.title.set('Departures')
+            break;
+          default:
+            this.title.set('Carpool')
+        }
+      })
+  }
+}
