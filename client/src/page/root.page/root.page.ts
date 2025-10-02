e45b386215cfb6b94ea312c16caba93c335b3b57
dfranz <dfranz@amanaacademy.org>
2025-10-02 13:11:09 -0400

    overscan

--- client/src/page/root.page/root.page.ts
+++ client/src/page/root.page/root.page.ts
@@ -1,13 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent, VersionComponent } from '../../component';
 import { ErrorComponent } from "../../component/error.component/error.component";
+import { Store } from '@ngrx/store';
+import { settingsSelectOverscan } from '../../store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'car-root',
-  imports: [ErrorComponent, HeaderComponent, RouterOutlet, VersionComponent],
+  imports: [AsyncPipe, ErrorComponent, HeaderComponent, RouterOutlet, VersionComponent],
   templateUrl: './root.page.html',
   styleUrl: './root.page.scss'
 })
 export class RootPage {
+  store = inject(Store)
+  overscan$ = this.store.select(settingsSelectOverscan)
 }
